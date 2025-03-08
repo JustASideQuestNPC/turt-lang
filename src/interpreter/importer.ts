88a2c99a87c6ab0579efb7c2da393bfaeda9c9ed
Joe Williams <joe.williams@oit.edu>
2025-03-08 13:32:50 -0800

    libraries can now only import functions

--- src/interpreter/importer.ts
+++ src/interpreter/importer.ts
@@ -2,24 +2,19 @@ import { TurtStdFunction } from "./callable.js";
 import Interpreter from "./interpreter.js";
 import { LiteralTypeUnion } from "./scanner.js";
 
-export type LibDataTypeUnion = ( number|string|boolean|null|
-    ((interpreter: Interpreter, ...args: LiteralTypeUnion[]) => LiteralTypeUnion)
+export type LibDataTypeUnion = (
+    (interpreter: Interpreter, ...args: LiteralTypeUnion[]) => LiteralTypeUnion
 );
 
 /**
- * Imports a set of functions or variables from a library.
+ * Imports a set of functions from a library.
  */
 export default function importLibrary(interpreter: Interpreter,
                                       libData: {[key: string]: LibDataTypeUnion}) {
     for (const [name, data] of Object.entries(libData)) {
         // convert functions to callable objects
-        if (typeof data === "function") {
-            // subtract 1 because the first parameter is the interpreter
-            const numArgs = data.length - 1;
-            interpreter.globals.define(name, new TurtStdFunction(name, numArgs, data));
-        }
-        else {
-            interpreter.globals.define(name, data);
-        }
+        // subtract 1 because the first parameter is the interpreter
+        const numArgs = data.length - 1;
+        interpreter.globals.define(name, new TurtStdFunction(name, numArgs, data));
     }
 }
